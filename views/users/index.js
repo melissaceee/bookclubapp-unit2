4d655c0506605f16ad5fbe0a9ebe0062159b6531
melissaceee <cheng.melissa@outlook.com>
2024-08-15 15:50:05 -0400

    update views users

---
+++ views/users/index.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Community Page</title>
+  <link rel="stylesheet" href="/styles.css">
+</head>
+<body>
+  <%- include('../partials/_navbar.ejs') %>
+  <h1>Our Members</h1>
+  <ul>
+    <% allUsers.forEach((user)=>{ %>
+      <li><h3><%= user.username %></h3></li>
+    <% }) %>
+  </ul>
+</body>
+</html>
