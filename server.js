8d17bd8aab6101ac2d3891d1a1c7d0e6780859b1
melissaceee <cheng.melissa@outlook.com>
2024-08-15 15:19:02 -0400

    update folders

---
+++ server.js
@@ -0,0 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const session = require('express-session');
