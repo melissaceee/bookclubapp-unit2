91bdb2187d0c2c50ff94ee4321a4728c5c6c0880
melissaceee <cheng.melissa@outlook.com>
2024-08-15 15:33:12 -0400

    books

--- server.js
+++ server.js
@@ -6,3 +6,49 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+const authController = require('./controllers/auth.js');
+const usersController = require('./controllers/users.js');
+const booksController = require('./controllers/books.js');
+const reviewsController = require("./controllers/reviews.js");
+
+const port = process.env.PORT ? process.env.PORT : '3000';
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', {
+    user: req.session.user,
+  });
+});
+
+app.use(express.static('public'));
+
+app.use(passUserToView);
+app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users', usersController);
+app.use('/users/:userId/books', booksController);
+app.use("/reviews", reviewsController);
+
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
